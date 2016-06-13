c97ab8d8085f640c0986212df6890961953d7ee7
Tim Hacker <timothyhacker@gmail.com>
2016-06-13 15:32:19 +0100

    Fixed routes by removing quotes

--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,8 @@ import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 
 export default (
-  <Route path="/" component="{App}">
-    <IndexRoute component="{HomePage}" />
-    <Route path="about" component="{AboutPage}" />
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage} />
+    <Route path="about" component={AboutPage} />
   </Route>
 );

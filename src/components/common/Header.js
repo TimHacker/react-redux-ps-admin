a1d2096d0dd073e9e47726e3700000b6c87e31f9
Tim Hacker <timothyhacker@gmail.com>
2016-06-13 16:30:50 +0100

    Switch Header nav to pure functional component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import {Link} from 'react-router';
+import {IndexLink, Link} from 'react-router';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div id="navbar" className="navbar-collapse collapse">
-            <ul className="nav navbar-nav">
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="about">About</Link></li>
-            </ul>
-          </div>
+const Header = () => {
+  return (
+    <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        <div id="navbar" className="navbar-collapse collapse">
+          <ul className="nav navbar-nav">
+            <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
+            <li><Link to="about" activeClassName="active">About</Link></li>
+          </ul>
         </div>
-      </nav>
-    );
-  }
-}
+      </div>
+    </nav>
+  );
+};
 
 export default Header;

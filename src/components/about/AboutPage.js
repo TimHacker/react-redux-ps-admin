13c17985442bcc56c4eac350f1104b069318a1a0
Tim Hacker <timothyhacker@gmail.com>
2016-06-13 15:31:45 +0100

    Fix link to home from AboutPage

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -7,7 +7,7 @@ class AboutPage extends React.Component {
       <div className="jumbotron">
         <h1>About Pluralsight Administration site</h1>
         <p>Why did we use React, Redux, React Router, ES6 and a variety of other helpful libraries</p>
-        <Link to="home" className="btn btn-primary btn-lg">Back to homepage</Link>
+        <Link to="/" className="btn btn-primary btn-lg">Back to homepage</Link>
       </div>
     );
   }

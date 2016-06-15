e69343f77fd574ee7182a82f70a1c2a9dcfb021b
Tim Hacker <timothyhacker@gmail.com>
2016-06-15 19:00:03 +0100

    Getting the course form working with binds
    This is resulting in an eslint warning but we will fix this shortly

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,11 +2,37 @@ import React from 'react';
 import {Link} from 'react-router';
 
 class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: { title: "" }
+    };
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({ course });
+  }
+
+  onClickSave() {
+    alert(`Saving ${this.state.course.title}`);
+  }
+
   render() {
     return (
       <div className="jumbotron">
         <h1>Courses</h1>
-        <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
+        <h2>Add course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange.bind(this)}
+          value={this.state.course.title} />
+        <input
+          type="submit"
+          value="save"
+          onClick={this.onClickSave.bind(this)} />
       </div>
     );
   }

14c910a06680fd5a7776bd4d738d19c4cc728fcf
Tim Hacker <timothyhacker@gmail.com>
2016-10-05 11:27:08 +0100

    Debuggers to show flow through redux

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -30,6 +30,7 @@ class CoursesPage extends React.Component {
   }
 
   render() {
+    debugger;
     return (
       <div className="jumbotron">
         <h1>Courses</h1>
@@ -54,6 +55,7 @@ CoursesPage.propTypes = {
 };
 
 function mapStateToProps(state, ownProps) {
+  debugger;
   return {
     courses: state.courses
   };

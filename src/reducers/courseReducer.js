cff5e0f0a1b00b4465ca5ee4d34f1c8b97d2bc2e
Tim Hacker <timothyhacker@gmail.com>
2016-10-05 10:40:24 +0100

    Corrected version using object assign and spread
    This means that we create a deep copy of the state, add the course and
    then return that.

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,8 +1,10 @@
 export default function courseReducer(state = [], action) {
   switch (action.type) {
     case 'CREATE_COURSE':
-      state.push(action.course);
-      return state;
+      return [...state,
+        Object.assign({}, action.course)
+      ];
+
     default:
       return state;
   }

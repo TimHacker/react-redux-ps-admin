d80d471126fe358d4b0a0e2fd4d6fe0328a0a950
Tim Hacker <timothyhacker@gmail.com>
2016-10-06 12:11:47 +0100

    Added the named member import for PropTypes

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
@@ -23,8 +23,8 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
-  courses: React.PropTypes.array.isRequired,
-  actions: React.PropTypes.object.isRequired
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
